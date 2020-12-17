04e2716c046b190f72472942a9418a3b1d693d0d
guyn <guyynir@gmail.com>
2020-12-17 13:09:37 +0200

    first commit, just to define the main files needed

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020 Dec 17 13:06
+
+@author: guyn
+
+get sources from the Fritz marshal, 
+run custom analysis on each, 
+assign some values and post them back to Fritz
+as Annotations on those sources
+
+INSTRUCTIONS:
+    You must create a "config.yaml" file and specify the functions you want to 
+    run on each source, as im the example given below. 
+    You must also create a file named "token" and copy your token from Fritz 
+    into this file. 
+    Both config.yaml and token should be ignored by git, so you can custumize 
+    and put your token in there without it being published. 
+
+"""
+
